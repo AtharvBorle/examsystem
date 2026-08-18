249d13a1e2b1f2319a8333a48e936e7c22d0d586
Atharv Borle <atharvabbvd@gmail.com>
2026-08-18 13:53:15 +0530

    chore: support multi-environment targets in ecosystem.config.js and deploy scripts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   apps: [
     {
+      name: 'bvpindia-backend',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 5000',
+      cwd: './backend',
+      instances: 'max',
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 5000
+      }
+    },
+    {
       name: 'bvp-exam-backend-dev',
       script: 'node_modules/next/dist/bin/next',
       args: 'start -p 5001',
